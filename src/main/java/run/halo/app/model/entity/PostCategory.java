8e64b5eafcae7929aa3911881db849a57ea2bf66
salahatwa <salahsayedatwa@gmail.com>
2022-01-11 13:52:55 +0200

    handle postgres dialict

--- src/main/java/run/halo/app/model/entity/PostCategory.java
+++ src/main/java/run/halo/app/model/entity/PostCategory.java
@@ -19,43 +19,41 @@ import java.util.Objects;
 @ToString(callSuper = true)
 @RequiredArgsConstructor
 @Entity
-@Table(name = "post_categories", indexes = {
-        @Index(name = "post_categories_post_id", columnList = "post_id"),
-        @Index(name = "post_categories_category_id", columnList = "category_id")})
+@Table(name = "post_categories", indexes = { @Index(name = "post_categories_post_id", columnList = "post_id"),
+		@Index(name = "post_categories_category_id", columnList = "category_id") })
 public class PostCategory extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "custom-id")
-    @GenericGenerator(name = "custom-id", strategy = "run.halo.app.model.entity.support.CustomIdGenerator")
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY, generator = "custom-id")
+	@GenericGenerator(name = "custom-id", strategy = "run.halo.app.model.entity.support.CustomIdGenerator")
+	private Integer id;
 
-    /**
-     * Category id.
-     */
-    @Column(name = "category_id")
-    private Integer categoryId;
+	/**
+	 * Category id.
+	 */
+	@Column(name = "category_id", columnDefinition = "integer")
+	private Integer categoryId;
 
-    /**
-     * Post id.
-     */
-    @Column(name = "post_id")
-    private Integer postId;
+	/**
+	 * Post id.
+	 */
+	@Column(name = "post_id", columnDefinition = "integer")
+	private Integer postId;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        PostCategory that = (PostCategory) o;
-        return categoryId.equals(that.categoryId) &&
-                postId.equals(that.postId);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		PostCategory that = (PostCategory) o;
+		return categoryId.equals(that.categoryId) && postId.equals(that.postId);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(categoryId, postId);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(categoryId, postId);
+	}
 }
